e6653daab82375da73c8bab25cecfb689486622a
ukozdan <danroysanders@gmail.com>
2019-10-17 15:54:02 +0100

    36 files modified

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home";
 import { AdminComponent } from "./admin";
 import { LoginComponent } from "./login";
-import { IncidentComponent } from "./incident";
-import { AssistanceComponent } from "./assistance";
-import { PenTestComponent } from "./pen-test";
-import { QuickCheckComponent } from "./quick-check";
+//import { IncidentComponent } from "./incident";
+//import { AssistanceComponent } from "./assistance";
+//import { PenTestComponent } from "./pen-test";
+//import { QuickCheckComponent } from "./quick-check";
 import { AuthGuard } from "./_helpers";
 import { Role } from "./_models";
 
