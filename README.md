9c64609a923cf5679fdb82b8df0cd6b0928c0c0a
ukozdan <danroysanders@gmail.com>
2019-10-17 14:29:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8-role-based-authorization-example-4rhs7w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8-role-based-authorization-example-4rhs7w)
---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent } from './app.component';
+import { appRoutingModule } from './app.routing';
+
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { AdminComponent } from './admin';
+import { LoginComponent } from './login';
+import { AssistanceComponent } from './assistance/assistance.component';
+import { IncidentComponent } from './incident/incident.component';
+import { PenTestComponent } from './pen-test/pen-test.component';
+import { QuickCheckComponent } from './quick-check/quick-check.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        AdminComponent,
+        LoginComponent,
+        AssistanceComponent,
+        IncidentComponent,
+        PenTestComponent,
+        QuickCheckComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
